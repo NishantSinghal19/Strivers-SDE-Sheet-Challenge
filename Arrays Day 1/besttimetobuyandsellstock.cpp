21f06228cdc63e45e1da18f2c23d83c428521c32
Nishant Singhal <81030284+NishantSinghal19@users.noreply.github.com>
2023-06-04 23:08:39 +0530

    Create besttimetobuyandsellstock.cpp

---
+++ Arrays Day 1/besttimetobuyandsellstock.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    // Write your code here.
+    int lsf=INT_MAX;
+    int op=0;
+    int pist=0;
+    for(int i=0;i<prices.size();i++){
+        if(prices[i]<lsf){
+            lsf=prices[i];
+        }
+        pist=prices[i]-lsf;
+        if(op<pist){
+            op=pist;
+        }
+    }
+    return op;
+}
