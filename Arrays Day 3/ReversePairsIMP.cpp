baa4a7edc5cc860a54b16ccdbf65e617cb0adcc0
Nishant Singhal <81030284+NishantSinghal19@users.noreply.github.com>
2023-06-06 22:22:32 +0530

    Create ReversePairsIMP.cpp

---
+++ Arrays Day 3/ReversePairsIMP.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h> 
+// int reversePairs(vector<int> &arr, int n){
+// 	// Write your code here.	
+// 	int pairs=0;
+// 	for(int i=0;i<arr.size();i++){
+// 		for (int j = i + 1; j < arr.size(); j++) {
+//           if (arr[i] > 2 * arr[j]) pairs++;
+//     	}
+//     }
+//     return pairs;
+// }
+int Merge(vector < int > & nums, int low, int mid, int high) {
+  int total = 0;
+  int j = mid + 1;
+  for (int i = low; i <= mid; i++) {
+    while (j <= high && nums[i] > (2 * (long)nums[j])) {
+      j++;
+    }
+    total += (j - (mid + 1));
+  }
+
+  vector < int > t;
+  int left = low, right = mid + 1;
+
+  while (left <= mid && right <= high) {
+
+    if (nums[left] <= nums[right]) {
+      t.push_back(nums[left++]);
+    } else {
+      t.push_back(nums[right++]);
+    }
+  }
+
+  while (left <= mid) {
+    t.push_back(nums[left++]);
+  }
+  while (right <= high) {
+    t.push_back(nums[right++]);
+  }
+
+  for (int i = low; i <= high; i++) {
+    nums[i] = t[i - low];
+  }
+  return total;
+}
+int MergeSort(vector < int > & nums, int low, int high) {
+
+  if (low >= high) return 0;
+  int mid = (low + high) / 2;
+  int inv = MergeSort(nums, low, mid);
+  inv += MergeSort(nums, mid + 1, high);
+  inv += Merge(nums, low, mid, high);
+  return inv;
+}
+int reversePairs(vector < int > & arr,int n) {
+  return MergeSort(arr, 0, n - 1);
+}
