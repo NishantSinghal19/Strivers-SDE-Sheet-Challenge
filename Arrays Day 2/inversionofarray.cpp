a751042d6ab566a8bdf51e9a13091212398d9c7e
Nishant Singhal <81030284+NishantSinghal19@users.noreply.github.com>
2023-06-06 21:27:43 +0530

    Create inversionofarray.cpp

---
+++ Arrays Day 2/inversionofarray.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+long long getInversions(long long *arr, int n){
+    // Write your code here.
+    long long ans=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]>arr[j] && i<j){
+                ans++;
+            }
+        }
+    }
+    return ans;
+}
