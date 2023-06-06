edd388896bb9a87c64afdf19f9b73205f59dd893
Nishant Singhal <81030284+NishantSinghal19@users.noreply.github.com>
2023-06-06 21:48:27 +0530

    Update majorityelement.cpp

--- Arrays Day 3/majorityelement.cpp
+++ Arrays Day 3/majorityelement.cpp
@@ -2,18 +2,10 @@
 
 int findMajorityElement(int arr[], int n) {
 	// Write your code here.
-	//size of the given array:
-
-
-    //declaring a map:
     map<int, int> mpp;
-
-    //storing the elements with its occurnce:
     for (int i = 0; i < n; i++) {
         mpp[arr[i]]++;
     }
-
-    //searching for the majority element:
     for (auto it : mpp) {
         if (it.second > (n / 2)) {
             return it.first;
