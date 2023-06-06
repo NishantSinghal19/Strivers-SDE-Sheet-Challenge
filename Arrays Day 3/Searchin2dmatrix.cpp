4349c5bd2de95bdbd1c73bbea4f136c4949f3978
Nishant Singhal <81030284+NishantSinghal19@users.noreply.github.com>
2023-06-06 16:00:28 +0000

    Array3

---
+++ Arrays Day 3/Searchin2dmatrix.cpp
@@ -0,0 +1,36 @@
+bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int n=mat.size();
+        int m=mat[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+}
+//o(m*n)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int lo = 0;
+        if(!matrix.size()) return false;
+        int hi = (matrix.size() * matrix[0].size()) - 1;
+        
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] == target) {
+                return true;
+            }
+            if(matrix[mid/matrix[0].size()][mid % matrix[0].size()] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        return false;
+    }
+};
+//o(log(m*n))
