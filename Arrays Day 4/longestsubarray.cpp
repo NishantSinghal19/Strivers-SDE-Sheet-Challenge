f5fb787a059638316776f99df52ce830cf48f4b3
Nishant Singhal <81030284+NishantSinghal19@users.noreply.github.com>
2023-06-07 22:53:04 +0530

    Create longestsubarray.cpp

---
+++ Arrays Day 4/longestsubarray.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+int LongestSubsetWithZeroSum(vector < int > arr) {
+
+  // Write your code here
+  unordered_map<int,int> mpp;
+  int maxi=0;
+  int sum=0;
+  int n= arr.size();
+  for(int i=0;i<n;i++){
+    sum+=arr[i];
+    if(sum==0){
+      maxi=i+1;
+    }else{
+      if(mpp.find(sum)!=mpp.end()){
+        maxi=max(maxi,i-mpp[sum]);
+      }else{
+        mpp[sum]=i;
+      }
+    }
+  }
+  return maxi;
+
+}
