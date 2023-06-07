c7b5db1099aa781c4c1cd55c5b8de80be95ef3bb
Nishant Singhal <81030284+NishantSinghal19@users.noreply.github.com>
2023-06-07 23:20:58 +0530

    Update longestsubstringIMP.cpp

--- Arrays Day 4/longestsubstringIMP.cpp
+++ Arrays Day 4/longestsubstringIMP.cpp
@@ -1,1 +1,19 @@
 
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string input)
+{
+    //Write your code here
+    vector<int> mpp(256,-1);
+    int l=0;int r=0;
+    int n=input.size();
+    int ans=0;
+    while(r<n){
+        if(mpp[input[r]]!=-1){
+            l= max(mpp[input[r]]+1,l);
+        }
+        mpp[input[r]]=r;
+        ans=max(ans,r-l+1);
+        r++;
+    }
+    return ans;
+}
